bfb3de64290f028e1c2ed4c5ee9ede6cbabc8989
elks <git@soderstrome.com>
2015-07-21 14:55:48 -0400

    150721 Removed unfinished JS

--- js/engage-global-scripts.js
+++
@@ -1,6 +0,0 @@
-var link = document.createElement('link');
-link.href = 'https://raw.githubusercontent.com/actcode/css/master/engage.css';
-link.rel = 'stylesheet';
-link.type = 'text/css';
-
-document.getElementsByTagName('head')[0].appendChild(link);
