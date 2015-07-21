36acd035bb355d9af5fb433aa512b6b329eb5001
elks <git@soderstrome.com>
2015-07-21 11:15:57 -0400

    First Commit

---
+++ js/engage-global-scripts.js
@@ -0,0 +1,6 @@
+var link = document.createElement('link');
+link.href = 'https://raw.githubusercontent.com/actcode/css/master/engage.css';
+link.rel = 'stylesheet';
+link.type = 'text/css';
+
+document.getElementsByTagName('head')[0].appendChild(link);
